8f2f8cfe13326213ac78b88df67a23a5241c2f9f
Onur YANAR <onur@optimum7.com>
2025-11-01 12:52:11 +0300

    chore: update plugins and add LazyVim extras
    - Update mason repository reference (williamboman -> mason-org)
    - Add prettierd formatter to mason ensure_installed
    - Enable 25 LazyVim extras for enhanced functionality:
      - Languages: ocaml, typescript, json, yaml, tailwind, git
      - Coding: mini-comment, mini-surround, neogen, yanky
      - Editor: dial, inc-rename, mini-diff, mini-files, outline
      - Formatting: prettier, eslint
      - UI: indent-blankline, mini-indentscope, treesitter-context
      - Utils: dot, gh, mini-hipatterns, project
    - Update plugin lockfile with new dependencies

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -40,13 +40,14 @@ return {
   },
 
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = {
       ensure_installed = {
         "stylua",
         "shellcheck",
         "shfmt",
         "flake8",
+        "prettierd",
       },
     },
   },
